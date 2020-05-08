0da215646d671df5701eb03e28397d0eca70515b
iamsaalim <saalimquadri1@gmail.com>
2020-05-08 11:12:11 +0530

    anykernel: checkout to old ak.sh * have some comments kek
    Signed-off-by: iamsaalim <saalimquadri1@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -5,7 +5,7 @@ properties() { '
 kernel.string=Kernel from Stormbreaker-projects
 do.devicecheck=1
 do.modules=0
-do.systemless=0
+do.systemless=1
 do.cleanup=1
 do.cleanuponabort=1
 device.name1=
@@ -14,11 +14,18 @@ s upported.versions=
 supported.patchlevels=
 '; }
 
-block=auto;
-is_slot_device=auto;
+# shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=0;
 ramdisk_compression=auto;
 
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference 
 . tools/ak3-core.sh;
 
+## AnyKernel install
 dump_boot;
+
+
 write_boot;
+## end install
