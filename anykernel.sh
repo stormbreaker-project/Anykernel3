6fa7391037defc9d8fb197af96efd471c1fc41cd
iamsaalim <saalimquadri1@gmail.com>
2020-06-30 08:41:32 +0200

    bringup phoenix
    Signed-off-by: iamsaalim <saalimquadri1@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -1,31 +1,48 @@
 # AnyKernel3 Ramdisk Mod Script
 # osm0sis @ xda-developers
 
+## AnyKernel setup
+# begin properties
 properties() { '
-kernel.string=Kernel from Stormbreaker-projects
+kernel.string=StormBreaker Kernel by CryllicBuster273
 do.devicecheck=1
 do.modules=0
-do.systemless=1
 do.cleanup=1
-do.cleanuponabort=1
-device.name1=
-device.name2=
+do.cleanuponabort=0
+device.name1=phoenix
+device.name2=phoenixin
+device.name3=
+device.name4=
+device.name5=
 supported.versions=
-supported.patchlevels=
-'; }
+'; } # end properties
 
 # shell variables
 block=/dev/block/bootdevice/by-name/boot;
-is_slot_device=0;
+is_slot_device=auto;
 ramdisk_compression=auto;
 
+
 ## AnyKernel methods (DO NOT CHANGE)
-# import patching functions/variables - see for reference 
+# import patching functions/variables - see for reference
 . tools/ak3-core.sh;
+
+
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+set_perm_recursive 0 0 750 750 $ramdisk/*;
+
 
 ## AnyKernel install
 dump_boot;
 
+# Begin Ramdisk Changes
+
+# migrate from /overlay to /overlay.d to enable SAR Magisk
+if [ -d $ramdisk/overlay ]; then
+  rm -rf $ramdisk/overlay;
+fi;
 
 write_boot;
 ## end install
+
