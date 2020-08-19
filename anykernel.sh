f7457c9315a7ab0ce618637518ccfc20f376961d
starlight5234 <ifist2834@gmail.com>
2020-08-20 01:34:23 +0530

    Update ASCII and Flash screen
    Signed-off-by: starlight5234 <ifist2834@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -1,8 +1,11 @@
 # AnyKernel3 Ramdisk Mod Script
 # osm0sis @ xda-developers
 
+## AnyKernel setup
+# begin properties
 properties() { '
-kernel.string=Kernel from Stormbreaker-projects
+kernel.string=StormBreaker Kernel
+maintainer.string1=Saalim Quadri, Team StormBreaker
 do.devicecheck=1
 do.modules=0
 do.systemless=1
@@ -12,16 +15,24 @@ device.name1=X01A
 device.name2=X01AD
 supported.versions=
 supported.patchlevels=
-'; }
+'; } # end properties
 
 # shell variables
 block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
 
+
 ## AnyKernel methods (DO NOT CHANGE)
-# import patching functions/variables - see for reference 
+# import patching functions/variables - see for reference
 . tools/ak3-core.sh;
+
+
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+set_perm_recursive 0 0 755 644 $ramdisk/*;
+set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+
 
 ## AnyKernel install
 dump_boot;
@@ -29,3 +40,4 @@ dump_boot;
 
 write_boot;
 ## end install
+
