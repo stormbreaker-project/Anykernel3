874e02ec4959a353173637236817459eabb34d1f
starlight5234 <ifist2834@gmail.com>
2020-08-20 01:34:23 +0530

    Update ASCII and Flash screen
    Signed-off-by: starlight5234 <ifist2834@gmail.com>
    
    [Forenche] Adjust anykernel.sh for rmx1851
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,9 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Stormbreaker kernel for Realme 3 Pro
+kernel.string=StormBreaker Kernel
+maintainer.string1=Forenche TG:@YouAFuckinHoe
+maintainer.string2=Saalim Quadri, Team StormBreaker Head
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
@@ -21,9 +23,17 @@ block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
 
+
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
+
+
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+set_perm_recursive 0 0 755 644 $ramdisk/*;
+set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+
 
 ## AnyKernel install
 dump_boot;
@@ -33,3 +43,4 @@ dump_boot;
 
 write_boot;
 ## end install
+
