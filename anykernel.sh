aad5f428dfe58d06b7d2d84f99b4959a0d01cd23
Rahul Pandey <61968330+Forenche@users.noreply.github.com>
2020-11-22 21:48:48 +0530

    anykernel.sh: Update for surya

--- anykernel.sh
+++ anykernel.sh
@@ -5,16 +5,13 @@
 # begin properties
 properties() { '
 kernel.string=StormBreaker Kernel
-maintainer.string1=Forenche TG:@YouAFuckinHoe
-maintainer.string2=Saalim Quadri, Team StormBreaker Head
+maintainer.string1=Forenche TG:@NubSusCat
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
 do.cleanuponabort=1
-device.name1=RMX1851
-device.name2=rmx1851
-device.name3=Realme 3 Pro
-device.name4=realme 3 pro
+device.name1=surya
+device.name2=karna
 supported.versions=10-11
 '; } # end properties
 
@@ -27,13 +24,6 @@ ramdisk_compression=auto;
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
-
-
-## AnyKernel file attributes
-# set permissions/ownership for included ramdisk files
-set_perm_recursive 0 0 755 644 $ramdisk/*;
-set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
-
 
 ## AnyKernel install
 dump_boot;
