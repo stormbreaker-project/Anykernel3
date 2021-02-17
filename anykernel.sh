a509243929206e2c7e5539e5407b23fe79b05aa9
ItsVixano <vixanonew@gmail.com>
2021-02-17 15:18:54 +0100

    ak3: Add tweaks for kernel
    * credits to:
    - @ darkhz for the initial idea
    - @ Lyceris-chan for ktweak modification
    - @ LinkBoi00 for stuneboost disabler
    
    Co-authored-by: darkhz <kmachanwenw@gmail.com>
    Co-authored-by: lyceris-chan <ghostdrain@outlook.com>
    Co-authored-by: LinkBoi00 <50962670+LinkBoi00@users.noreply.github.com>
    Change-Id: I3e6f97278bac1b29f70f72724f0bef5f289dafde

--- anykernel.sh
+++ anykernel.sh
@@ -27,17 +27,38 @@ ramdisk_compression=auto;
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
 
+## General tuning
+# Let's install the script
+ui_print "- Installing on-boot execution script"
+mount -o remount,rw /vendor
+cp /tmp/anykernel/tools/ysl_exec.sh /vendor/bin/
+chmod u+x /vendor/bin/ysl_exec.sh
+
+# Check if the script is present
+is_exist_post_sh=$(grep ysl_exec /vendor/bin/init.qcom.post_boot.sh)
+is_exist_qcom_sh=$(grep ysl_exec /vendor/bin/init.qcom.sh)
+
+if [[ -z "$is_exist_post_sh" ]]; then
+    echo "/vendor/bin/ysl_exec.sh" >> /vendor/bin/init.qcom.post_boot.sh
+    ui_print "- Script installed on /vendor/bin/init.qcom.post_boot.sh"
+else
+    ui_print "- Script already exist on /vendor/bin/init.qcom.post_boot.sh"
+fi
+
+if [[ -z "$is_exist_qcom_sh" ]]; then
+    echo "/vendor/bin/ysl_exec.sh" >> /vendor/bin/init.qcom.sh
+    ui_print "- Script installed on /vendor/bin/init.qcom.sh"
+else
+    ui_print "- Script already exist on /vendor/bin/init.qcom.sh"
+fi
 
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
 set_perm_recursive 0 0 755 644 $ramdisk/*;
 set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 
-
 ## AnyKernel install
 dump_boot;
 
-
 write_boot;
 ## end install
-
