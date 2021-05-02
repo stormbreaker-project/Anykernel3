cc25c3f58502209d7ce1e7061c30996e419d678b
MASTERGUY <murtipipo@gmail.com>
2021-05-02 14:02:46 +0530

    anykernel.sh: Sweetify
    Signed-off-by: MASTERGUY <murtipipo@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -5,14 +5,14 @@
 # begin properties
 properties() { '
 kernel.string=StormBreaker Kernel
-maintainer.string1=Forenche TG:@rakoool
+maintainer.string1=Murtaza Piplod TG:@mtpiplod XDA:@murtaza1326
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
 do.cleanuponabort=1
-device.name1=surya
-device.name2=karna
-supported.versions=10-11
+device.name1=sweet
+device.name2=sweetin
+supported.versions=11
 '; } # end properties
 
 # shell variables
